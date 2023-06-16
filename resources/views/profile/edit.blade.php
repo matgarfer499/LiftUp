3ab60f176a1640e59646ccf460a6ffa80d5a23d4
mgarfer499 <115544026+mgarfer499@users.noreply.github.com>
2023-06-16 22:38:23 +0200

    configuracion perfil usuario

--- resources/views/profile/edit.blade.php
+++ resources/views/profile/edit.blade.php
@@ -1,29 +1,75 @@
-<x-app-layout>
-    <x-slot name="header">
-        <h2 class="font-semibold text-xl text-gray-800 dark:text-gray-200 leading-tight">
-            {{ __('Profile') }}
-        </h2>
-    </x-slot>
+@extends('layouts.plantilla')
 
-    <div class="py-12">
-        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8 space-y-6">
-            <div class="p-4 sm:p-8 bg-white dark:bg-gray-800 shadow sm:rounded-lg">
-                <div class="max-w-xl">
-                    @include('profile.partials.update-profile-information-form')
-                </div>
-            </div>
+@section('title', 'Perfil')
 
-            <div class="p-4 sm:p-8 bg-white dark:bg-gray-800 shadow sm:rounded-lg">
-                <div class="max-w-xl">
-                    @include('profile.partials.update-password-form')
-                </div>
-            </div>
+@section('scripts')
+@parent
+@endsection
 
-            <div class="p-4 sm:p-8 bg-white dark:bg-gray-800 shadow sm:rounded-lg">
-                <div class="max-w-xl">
-                    @include('profile.partials.delete-user-form')
-                </div>
-            </div>
+@section('content')
+<div class="w-11/12 m-auto mb-4 flex flex-col">
+    <h2 class="text-2xl font-semibold text-start">CONFIGURACIÓN DE PERFIL</h2>
+    <h4 class="text-lg font-semibold text-gray-500">Bienvenido {{auth()->user()->first_name}} {{auth()->user()->last_name}}</h4>
+</div>
+<div class="w-11/12 m-auto flex justify-start gap-2 relative">
+    <div class="w-2/12 relative p-4 border-2 border-gray-300 rounded-md shadow-sm shadow-gray-500">
+        <h4 class="font-semibold text-md text-gray-500 mb-2 text-center">Foto de perfil</h4>
+        <div class="group flex items-center justify-center">
+            <img src="{{auth()->user()->profile_picture}}" class="w-[120px] h-[120px] object-cover rounded-full" alt=""></img>
+            <button class="absolute px-4 py-1 border-black border-[1px] top-32 left-15 text-white bg-black group-hover:opacity-90 opacity-0 duration-500 ease-in-out rounded-md">Editar</button>
         </div>
     </div>
-</x-app-layout>
+    <form method="post" action="{{ route('profile.update') }}" class="border-2 border-gray-300 rounded-md w-full p-4 flex justify-between flex-wrap shadow-sm shadow-gray-500">
+        @csrf
+        @method('patch')
+        <div class="w-full flex flex-col">
+            <h2 class="text-xl font-semibold">Información básica del perfil</h2>
+        </div>
+        <div class="w-6/12">
+            <div class="flex flex-col gap-1">
+                <label class="text-gray-500 text-sm font-semibold" for="first_name">Nombre</label>
+                <input type="text" id="first_name" name="first_name" class="rounded-md border-gray-500 border-2" value="{{auth()->user()->first_name}}">
+                @error('first_name') <span class="text-red-500">{{ $message }}</span> @enderror
+                <label class="text-gray-500 text-sm font-semibold" for="last_name">Apellido</label>
+                <input type="text" id="last_name" name="last_name" class="rounded-md border-gray-500 border-2" value="{{auth()->user()->last_name}}">
+                @error('last_name') <span class="text-red-500">{{ $message }}</span> @enderror
+            </div>
+        </div>
+        <div class="w-5/12">
+            <div class="flex flex-col gap-1">
+                <label class="text-gray-500 text-sm font-semibold" for="email">Email</label>
+                <input type="text" id="email" name="email" class="rounded-md border-gray-500 border-2" value="{{auth()->user()->email}}">
+                @error('email') <span class="text-red-500">{{ $message }}</span> @enderror
+                <label for="" class="opacity-0">boton de guardar</label>
+                <button type="submit" class="w-full rounded-md bg-black text-white font-medium py-2">Guardar</button>
+            </div>
+        </div>
+    </form>
+</div>
+<div class="w-11/12 m-auto mt-4 flex justify-between gap-2 mb-4">
+    <form method="post" action="{{ route('password.update') }}" class="w-4/5 p-4 border-2 border-gray-300 rounded-md shadow-sm shadow-gray-500">
+        @csrf
+        @method('put')
+        <h2 class="text-xl font-semibold text-center">Actualizar contraseña</h2>
+        <div class="flex flex-col gap-4">
+            <label for="current_password" class="text-gray-500 text-sm font-semibold" for="">Contraseña actual</label>
+            <input id="current_password" name="current_password" type="password" class="rounded-md border-gray-500 border-2">
+            @error('current_password') <span class="text-red-500">{{ $message }}</span> @enderror
+            <label for="password" class="text-gray-500 text-sm font-semibold" for="">Nueva contraseña</label>
+            <input id="password" name="password" type="password" class="rounded-md border-gray-500 border-2">
+            <label for="password_confirmation" class="text-gray-500 text-sm font-semibold" for="">Repita su nueva contraseña</label>
+            <input id="password_confirmation" name="password_confirmation" type="password" class="rounded-md border-gray-500 border-2">
+            <button type="submit" class="py-2 bg-black text-white rounded-md w-1/4 m-auto">guardar</button>
+        </div>
+    </form>
+    <div class="w-1/5 border-2 border-gray-300 rounded-md shadow-sm shadow-gray-500 h-36 p-4">
+        <div class="flex flex-col gap-4">
+            <form method="POST" action="{{ route('logout') }}" class="m-auto w-3/4">
+                @csrf
+                <button type="submit" class="py-2 bg-black text-white rounded-md w-full m-auto">Cerrar sesión</button>
+            </form>
+            @include('profile.partials.delete-user-form')
+        </div>
+    </div>
+</div>
+@endsection
--- resources/views/profile/partials/delete-user-form.blade.php
+++ resources/views/profile/partials/delete-user-form.blade.php
@@ -1,18 +1,9 @@
-<section class="space-y-6">
-    <header>
-        <h2 class="text-lg font-medium text-gray-900 dark:text-gray-100">
-            {{ __('Delete Account') }}
-        </h2>
-
-        <p class="mt-1 text-sm text-gray-600 dark:text-gray-400">
-            {{ __('Once your account is deleted, all of its resources and data will be permanently deleted. Before deleting your account, please download any data or information that you wish to retain.') }}
-        </p>
-    </header>
-
-    <x-danger-button
+<section class="m-auto w-3/4">
+    <button
+        class="m-auto py-2 bg-red-500 text-white rounded-md w-full"
         x-data=""
         x-on:click.prevent="$dispatch('open-modal', 'confirm-user-deletion')"
-    >{{ __('Delete Account') }}</x-danger-button>
+    >{{ __('Eliminar cuenta') }}</button>
 
     <x-modal name="confirm-user-deletion" :show="$errors->userDeletion->isNotEmpty()" focusable>
         <form method="post" action="{{ route('profile.destroy') }}" class="p-6">
@@ -20,11 +11,11 @@
             @method('delete')
 
             <h2 class="text-lg font-medium text-gray-900 dark:text-gray-100">
-                {{ __('Are you sure you want to delete your account?') }}
+                {{ __('¿Seguro que quieres eliminar tu cuenta?') }}
             </h2>
 
             <p class="mt-1 text-sm text-gray-600 dark:text-gray-400">
-                {{ __('Once your account is deleted, all of its resources and data will be permanently deleted. Please enter your password to confirm you would like to permanently delete your account.') }}
+                {{ __('Una vez eliminada su cuenta, todos sus recursos y datos se borrarán permanentemente. Introduzca su contraseña para confirmar que desea eliminar definitivamente su cuenta.') }}
             </p>
 
             <div class="mt-6">
@@ -35,7 +26,7 @@
                     name="password"
                     type="password"
                     class="mt-1 block w-3/4"
-                    placeholder="Password"
+                    placeholder="Contraseña"
                 />
 
                 <x-input-error :messages="$errors->userDeletion->get('password')" class="mt-2" />
@@ -43,11 +34,11 @@
 
             <div class="mt-6 flex justify-end">
                 <x-secondary-button x-on:click="$dispatch('close')">
-                    {{ __('Cancel') }}
+                    {{ __('Cancelar') }}
                 </x-secondary-button>
 
                 <x-danger-button class="ml-3">
-                    {{ __('Delete Account') }}
+                    {{ __('Eliminar') }}
                 </x-danger-button>
             </div>
         </form>
